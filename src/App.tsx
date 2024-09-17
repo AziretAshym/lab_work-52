28c895c8608015c06745fcfe9f0dfd2d39d09a72
Ashymov Aziret <aziretashym@gmail.com>
2024-09-17 15:43:15 +0600

    Addition: to prevent a new deck from being created when pressing the "dealBtn" button, the "getRestOfCards" method was added to track the remaining cards in the deck and display the rest cards.

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,43 @@ import './App.css';
 import {useState} from "react";
 import ClassCard from "./lib/classCard.ts";
 import classCardDeck from "./lib/classCardDeck.ts";
+import classCard from "./lib/classCard.ts";
 
 
 const App = () => {
-    const [hand, setHand] = useState<ClassCard[]>([]);
-
+    const [deck, setDeck] = useState<classCardDeck | null>(new classCardDeck());
+    const [cards, setCards] = useState<classCard[]>([]);
+    const [restOfCards, setRestOfCards] = useState<number>(52);
 
     const dealCards = () => {
-        const deck = new classCardDeck();
-        const dealtCards = deck.getCards(5);
-        setHand(dealtCards);
-    }
+        if (deck) {
+            const dealtCards = deck.getCards(5);
+            setCards(dealtCards);
+            setRestOfCards(deck.getRestOfCards());
+
+            if (dealtCards.length === 0) {
+                setDeck(null);
+            }
+        }
+    };
+
 
     return (
         <div className="container">
-            {hand.length > 0 && (
+            <span>Card in the deck: {restOfCards}</span>
+            {cards.length > 0 && (
                 <div className="playingCards faceImages">
-                    {hand.map((card, index) => (
+                    {cards.map((card, index) => (
                         <span key={index} className={`card rank-${card.rank.toLowerCase()} ${card.suit}`}>
                           <span className="rank">{card.rank}</span>
                             <span className="suit">
-                                {card.suit === 'diams' ? '♦' :
-                                    card.suit === 'hearts' ? '♥' :
-                                        card.suit === 'clubs' ? '♣' : '♠'}
+                                {card.suit === 'diams' ? '♦' : card.suit === 'hearts' ? '♥' : card.suit === 'clubs' ? '♣' : '♠'}
                           </span>
                         </span>
                     ))}
                 </div>
             )}
-            <button onClick={dealCards} className="dealBtn">Deal cards</button>
+            <button onClick={dealCards} disabled={!deck} className="dealBtn">{deck ? "Deal cards" : "The deck is empty"}</button>
         </div>
     );
 };
--- src/lib/classCardDeck.ts
+++ src/lib/classCardDeck.ts
@@ -16,6 +16,10 @@ class ClassCardDeck {
     }
 
     getCard(): ClassCard {
+        if (this.deck.length === 0) {
+            return null;
+        }
+
         const randomCard = Math.floor(Math.random() * this.deck.length);
         return this.deck.splice(randomCard, 1)[0];
     };
@@ -23,11 +27,19 @@ class ClassCardDeck {
     getCards(howMany: number): ClassCard[] {
         const cards: ClassCard[] = [];
         for (let i= 0; i < howMany; i++) {
-            cards.push(this.getCard());
+            const card = this.getCard();
+            if (card) {
+                cards.push(card);
+            } else {
+                break;
+            }
         }
         return cards;
-    }
+    };
 
+    getRestOfCards(): number {
+        return this.deck.length
+    }
 }
 
 export default ClassCardDeck;
